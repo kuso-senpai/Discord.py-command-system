3b70788f86497a4021dfe4754b025e2ef455db09
kuso-senpai <50300929+404kuso@users.noreply.github.com>
2021-05-06 12:07:30 +0200

    Update sample.py

--- commands/sample.py
+++ commands/sample.py
@@ -1,17 +1,31 @@
-#region imports
-from typing import List
+import time
+from datetime import datetime
+
 import discord
-from config import color
-#endregion
+from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext
 
-async def main(message: discord.Message, args: List[str], client: discord.Client):
-    await message.channel.send(embed=discord.Embed(description="This is a sample command", color=color))
+def register(client: commands.Bot):
+    slash: SlashCommand = client.slash
+    @slash.subcommand(base='sora', name="test", description="Testet den Bot")
+    async def command(ctx: SlashContext):
+        try:
+            await ctx.respond(eat=True)
+            x = current_milli_time()
+            msg = await ctx.send("hmmm...")
+            y = (current_milli_time() - x)
+            await msg.delete()
 
+            x_e= current_milli_time()
+            msg = await ctx.send("hmmm schon wieder?")
+            await msg.edit(content="ok.")
+            y_e = current_milli_time() - x_e
+            await msg.delete()
+            await ctx.send(f"`{y}ms` gebraucht um zu senden\n`{y_e}ms` gebraucht zum editieren")
+        except Exception as ex:
+            print(
+                f"\n{type(ex).__name__}: {ex}\n",
+                f"{__file__}:{ex.__traceback__.tb_lineno}"
+            )
 
-#region commandInfos needed
-name = "sampleCommand"                                          # Name
-desc = "Das hier ist ein Beispiel Befehl"                       # Beschreibung
-alias = ["sc", "sample"]                                        # Alias
-usage = "sampleCommand"                                         # Benutzung
-examples = ["sc", "sampleCommand"]                              # Beispiele
-#endregion
+current_milli_time = lambda: round(time.time() * 1000)
