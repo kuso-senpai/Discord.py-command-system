b46ca71f1a906a521fb0ff0566bed51ce1c66ea1
kuso-senpai <50300929+404kuso@users.noreply.github.com>
2021-05-06 12:11:16 +0200

    Update sample.py

--- commands/sample.py
+++ commands/sample.py
@@ -1,31 +1,9 @@
-import time
-from datetime import datetime
-
 import discord
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 
 def register(client: commands.Bot):
     slash: SlashCommand = client.slash
-    @slash.subcommand(base='sora', name="test", description="Testet den Bot")
+    @slash.subcommand(base='bot', name="test", description="Testet den Bot")
     async def command(ctx: SlashContext):
-        try:
-            await ctx.respond(eat=True)
-            x = current_milli_time()
-            msg = await ctx.send("hmmm...")
-            y = (current_milli_time() - x)
-            await msg.delete()
-
-            x_e= current_milli_time()
-            msg = await ctx.send("hmmm schon wieder?")
-            await msg.edit(content="ok.")
-            y_e = current_milli_time() - x_e
-            await msg.delete()
-            await ctx.send(f"`{y}ms` gebraucht um zu senden\n`{y_e}ms` gebraucht zum editieren")
-        except Exception as ex:
-            print(
-                f"\n{type(ex).__name__}: {ex}\n",
-                f"{__file__}:{ex.__traceback__.tb_lineno}"
-            )
-
-current_milli_time = lambda: round(time.time() * 1000)
+        await ctx.send("Erfolgreich")
